1fcebd04654c4c23e5fc43e6c9dae0f1882b1883
Ki <49635216+TurquoiseKitty@users.noreply.github.com>
2019-05-23 19:07:42 +0800

    Update math_cal.h

--- math_cal.h
+++ math_cal.h
@@ -5,8 +5,19 @@
 #include <vector>
 using namespace std;
 //with elements input and result output
-string CAL_alg(vector<string> &hold_strs, string type1){
+template <typename TY>
+string CAA(vector<string> & hold_strs, string type_nami){
+	
+	
+}
 
+
+string CAL_alg(vector<string> &hold_strs, string type){
+	//if any error occurs, return "NULL"
+	if(type!="int(11)"&&type!="double")return "NULL";
+	else if(type=="int(11)")return CAA<int>(hold_strs,type);
+	else return CAA<double>(hold_strs,type);
+	
 }
 
 #endif
