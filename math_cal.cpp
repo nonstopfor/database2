604b38ef5e1c159e3e7566ccfb3b8ce6a23ef6c5
Ki <49635216+TurquoiseKitty@users.noreply.github.com>
2019-06-10 01:03:18 +0800

    Update math_cal.cpp

--- math_cal.cpp
+++ math_cal.cpp
@@ -1,16 +1,34 @@
 #include "math_cal.h"
 
-string CAL_alg(vector<string> &hold_strs, string type){
+string CAL_alg(vector<string> &hold_strs, string type, bool& NULL_flag){
+//cout<<"meet cal"<<endl;
 	//if any error occurs, return "NULL"
     if(type == "date" || type == "time") return hold_strs[0];
-	if(type=="char(1)")return CAA_2(hold_strs);
-	else if(type!="int(11)"&&type!="double")return "NULL";
-	else if(type=="int(11)")return CAA<int>(hold_strs);
-	else return CAA<double>(hold_strs);
+	if(type=="char(1)"){
+        if(CAA_2(hold_strs,NULL_flag)=="NULL")NULL_flag=true;
+        return CAA_2(hold_strs,NULL_flag);
+    }
+    
+	else if(type!="int(11)"&&type!="double"){
+        NULL_flag=true;
+        return "NULL";
+    }
+	else if(type=="int(11)"){
+        if(CAA<int>(hold_strs,NULL_flag)=="NULL"){
+//cout<<"one NULL"<<endl;
+            NULL_flag=true;
+        }
+        return CAA<int>(hold_strs,NULL_flag);
+    }
+	else{
+        if(CAA<double>(hold_strs,NULL_flag)=="NULL")NULL_flag=true;
+        return CAA<double>(hold_strs,NULL_flag);
+    }
 	
 }
 
-string CAA_2(vector<string> & hold_strs){
+string CAA_2(vector<string> & hold_strs,bool& NULL_flag){
+    if(NULL_flag)return "NULL";
     int str_turn=1;
     int cal_turn=0;
     string resu;
